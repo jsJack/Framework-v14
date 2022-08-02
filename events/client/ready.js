537fdb1726b76da4fe836494a7875c45ca198a68
Jack <55438332+InfinityJxck@users.noreply.github.com>
2022-08-02 04:36:31 +0100

    embed for cooldowns, chalk and autochanging presences

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,5 +1,6 @@
 const { Client } = require("discord.js");
 const consola = require("consola");
+const chalk = require('chalk');
 
 module.exports = {
   name: "ready",
@@ -9,6 +10,35 @@ module.exports = {
    * @param {Client} client
    */
   execute(client) {
-    consola.success(`Hello world! Connected to Discord: ${client.user.tag}`);
+    consola.info(chalk.greenBright(`Hello world!`), chalk.gray(`Connected to Discord as`), chalk.blueBright(`${client.user.tag}`));
+    
+    await client.user.setActivity('Starting up... 🔴', {type: 'WATCHING'});
+    await client.user.setStatus("dnd");
+
+    if(client.config.mongoURL) {
+      await mongoose.connect(l.mongo, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }).then(() => { consola.info(`Connected to MongoDB: ${mongoose.connection.name}`);
+      }).catch((err) => { console.log(err); });
+    };
+
+    await client.user.setStatus("online");
+    await client.user.setActivity(`you. 👁👄👁`, {type: 'WATCHING'});
+
+    let state = 0;
+    let presences = [
+      { type: 'PLAYING',  message: 'play.minespells.com 🧙‍♂️'  },
+      { type: 'WATCHING', message: 'store.minespells.com 🛒' },
+      { type: 'LISTENING', message: 'minespells.com 💬' },
+      { type: 'COMPETING', message: 'the new Prisons map 🥇' },
+      { type: 'WATCHING', message: 'for the payouts 💎' },
+    ];
+
+    setInterval(() => {
+      state = (state + 1) % presences.length;
+      let presence = presences[state];
+      client.user.setActivity(presence.message, { type: presence.type });
+    }, 10000);
   },
 };
--- events/interaction/commandInteraction.js
+++ events/interaction/commandInteraction.js
@@ -1,4 +1,4 @@
-const { CommandInteraction, Collection } = require('discord.js');
+const { CommandInteraction, Collection, EmbedBuilder } = require('discord.js');
 const consola = require('consola');
 const ms = require('ms');
 
@@ -17,13 +17,18 @@ module.exports = {
         if (!command) return interaction.reply({ content: `There is no command with this name.`, ephemeral: true });
 
         if(command.cooldown) {
-            console.log(ms(command.cooldown));
-
             if(Timeout.has(`${interaction.commandName}${interaction.member.id}`)) {
                 let lastUsage = Timeout.get(`${interaction.commandName}${interaction.member.id}`);
                 let msTimeout = ms(command.cooldown) / 1000;
                 let timestamp = parseInt(lastUsage) + parseInt(msTimeout);
-                return interaction.reply({ content: `You can use this command again: <t:${timestamp}:R>` })
+
+                let cooldownEmbed = new EmbedBuilder()
+                .setTitle(`🏃‍♂️💨 Woah! Slow down!`)
+                .setDescription(`You are currently on a __cooldown__ for **/${interaction.commandName}**!\nYou can use the command again <t:${timestamp}:R>`)
+                .setColor(client.config.color)
+
+                consola.log(`${interaction.guild.name} | ${interaction.user.tag} | 🕕 Tried to use /${interaction.commandName} but is on cooldown.`)
+                return interaction.reply({ embeds: [cooldownEmbed], ephemeral: true });
             }
             
             Timeout.set(`${interaction.commandName}${interaction.member.id}`, (Date.now() / 1000).toFixed(0));
@@ -33,7 +38,7 @@ module.exports = {
             }, ms(command.cooldown));
         };
 
-        consola.info(`${interaction.guild.name} | ${interaction.member.user.tag} | /${interaction.commandName}`);
+        consola.log(`${interaction.guild.name} | ${interaction.member.user.tag} | /${interaction.commandName}`);
         command.execute(interaction, client);
     }
 }
