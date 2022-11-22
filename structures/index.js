b250b85c365144aa84e7a8543c1b2c32bfb3d5e1
Jack <55438332+InfinityJxck@users.noreply.github.com>
2022-11-22 20:23:30 +0000

    tweak(client): Add more intents for new features

--- structures/index.js
+++ structures/index.js
@@ -1,19 +1,23 @@
 const { Client, GatewayIntentBits, Partials, Collection } = require("discord.js");
-const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
-const { User, Message, GuildMember, ThreadMember } = Partials;
+const { Guilds, GuildMembers, GuildMessages, MessageContent, GuildVoiceStates, GuildMessageReactions } = GatewayIntentBits;
+const { User, Message, GuildMember, ThreadMember, Channel } = Partials;
 
 const consola = require('consola');
 
 const client = new Client({
-    intents: [Guilds, GuildMembers, GuildMessages],
-    partials: [User, Message, GuildMember, ThreadMember],
+    intents: [Guilds, GuildMembers, GuildMessages, MessageContent, GuildVoiceStates, GuildMessageReactions],
+    partials: [User, Message, GuildMember, ThreadMember, Channel],
 });
 
 client.events = new Collection();
 client.commands = new Collection();
 client.modals = new Collection();
 client.buttons = new Collection();
+client.selectmenus = new Collection();
 client.config = require("./config.json");
+client.customEmbedService = require('./funcs/tools/embedTools.js');
+
+module.exports = client;
 
 const { loadEvents } = require("./handlers/loadEvents");
 
