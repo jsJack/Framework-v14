872ced97fcb2e82147f4c666d14f9a37e4df50c5
Jack <55438332+InfinityJxck@users.noreply.github.com>
2023-04-09 23:12:56 +0100

    lint: fix spacing / tabs

--- structures/funcs/interaction/executeSlashCommand.js
+++ structures/funcs/interaction/executeSlashCommand.js
@@ -14,79 +14,73 @@ const cmdTimeout = new Collection();
 async function executeSlashCommand(interaction, client) {
     if (!interaction.isChatInputCommand()) return;
 
-        /********************************
-         * Check if the command exists  *
-         ********************************/
-        const command = client.commands.get(interaction.commandName);
+    /********************************
+     * Check if the command exists  *
+     ********************************/
+    const command = client.commands.get(interaction.commandName);
 
-        let notExist = new EmbedBuilder()
+    let notExist = new EmbedBuilder()
         .setDescription(`🛠 This command is not linked to a response.\nPlease try again later.`)
         .setColor(client.config.color)
-        
-        if (!command) return interaction.reply({ embeds: [notExist], ephemeral: true });
-
-        /****************************************************************
-         * Check if the database is on (for commands that need the db)  *
-         ****************************************************************/
-        if (client.config.mongoURL && command.dbDepend && connection.readyState != 1) {
-            let noDB = new EmbedBuilder()
         .setFooter({ text: `Item code: ${interaction.commandName} - Matrix Digital` });
+
+    if (!command) return interaction.reply({ embeds: [notExist], ephemeral: true });
+
+    /****************************************************************
+     * Check if the database is on (for commands that need the db)  *
+     ****************************************************************/
+    if (client.config.mongoURL && command.dbDepend && connection.readyState != 1) {
+        let noDB = new EmbedBuilder()
             .setTitle(`🌌 Hold on!`)
             .setDescription(`The database isn't quite connected yet, and you cannot use this command without the database.\nThe bot may be starting up, please allow up to 30 seconds before re-running this command.`)
             .setColor(client.config.color)
             .setFooter({ text: `Matrix Digital` });
 
-            consola.log(`${interaction.guild.name} | ${interaction.user.tag} | 💿 Tried to use /${interaction.commandName} but the database is not connected.`)
-            return interaction.reply({ embeds: [noDB], ephemeral: true });
-        }
+        consola.log(`${interaction.guild.name} | ${interaction.user.tag} | 💿 Tried to use /${interaction.commandName} but the database is not connected.`)
+        return interaction.reply({ embeds: [noDB], ephemeral: true });
+    }
 
-        /*************************************
-         * Check if user has required roles  *
-         *************************************/
-        if (command.reqRoles && !interaction.channel.isDMBased()) {
-            hasReqRole = false;
-            command.reqRoles.forEach((findRole) => {
-              if (interaction.member.roles.cache.some((role) => role.id === findRole)) hasReqRole = true;
-            });
+    /*************************************
+     * Check if user has required roles  *
+     *************************************/
+    if (command.reqRoles && !interaction.channel.isDMBased()) {
+        hasReqRole = false;
+        command.reqRoles.forEach((findRole) => {
+            if (interaction.member.roles.cache.some((role) => role.id === findRole)) hasReqRole = true;
+        });
 
-            let notReqRoles = new EmbedBuilder()
-              .setTitle(`❌ You do not have the required roles to execute this command`)
-              .setDescription(`Required Roles: <@&${command.reqRoles.join(">, <@&")}>`)
-              .setColor(client.config.color);
-      
-            if (!hasReqRole) return interaction.reply({ embeds: [notReqRoles], ephemeral: true });
-        }
+        let notReqRoles = new EmbedBuilder()
+            .setTitle(`❌ You do not have the required roles to execute this command`)
+            .setDescription(`Required Roles: <@&${command.reqRoles.join(">, <@&")}>`)
+            .setColor(client.config.color);
 
-        /*********************************
-         * Check if user is on cooldown  *
-         *********************************/
-        if (command.cooldown) {
-            if(cmdTimeout.has(`${interaction.commandName}${interaction.user.id}`)) {
-                let lastUsage = cmdTimeout.get(`${interaction.commandName}${interaction.user.id}`);
-                let msTimeout = ms(command.cooldown) / 1000;
-                let timestamp = parseInt(lastUsage) + parseInt(msTimeout);
+        if (!hasReqRole) return interaction.reply({ embeds: [notReqRoles], ephemeral: true });
+    }
 
-                let cooldownEmbed = new EmbedBuilder()
+    /*********************************
+     * Check if user is on cooldown  *
+     *********************************/
+    if (command.cooldown) {
+        if (cmdTimeout.has(`${interaction.commandName}${interaction.user.id}`)) {
+            let lastUsage = cmdTimeout.get(`${interaction.commandName}${interaction.user.id}`);
+            let msTimeout = ms(command.cooldown) / 1000;
+            let timestamp = parseInt(lastUsage) + parseInt(msTimeout);
+
+            let cooldownEmbed = new EmbedBuilder()
                 .setTitle(`🏃‍♂️💨 Woah! Slow down!`)
                 .setDescription(`You are currently on a __cooldown__ for **/${interaction.commandName}**!\nYou can use the command again <t:${timestamp}:R>`)
                 .setColor(client.config.color)
 
-                consola.log(`${interaction.guild.name} | ${interaction.user.tag} | 🕕 Tried to use /${interaction.commandName} but is on cooldown.`)
-                return interaction.reply({ embeds: [cooldownEmbed], ephemeral: true });
-            }
-            
-            cmdTimeout.set(`${interaction.commandName}${interaction.user.id}`, (Date.now() / 1000).toFixed(0));
+            consola.log(`${interaction.guild.name} | ${interaction.user.tag} | 🕕 Tried to use /${interaction.commandName} but is on cooldown.`)
+            return interaction.reply({ embeds: [cooldownEmbed], ephemeral: true });
+        }
 
-            setTimeout(() => {
-                cmdTimeout.delete(`${interaction.commandName}${interaction.user.id}`)
-            }, ms(command.cooldown));
-        };
+        cmdTimeout.set(`${interaction.commandName}${interaction.user.id}`, (Date.now() / 1000).toFixed(0));
 
-        /******************************
-         * Log & execute the command  *
-         ******************************/
-        consola.log(`${interaction.channel.isDMBased() ? `DMs` : `${interaction.guild.name}`} | ${interaction.user.tag} | /${interaction.commandName}`);
-        command.execute(interaction, client);
-}
+        setTimeout(() => {
+            cmdTimeout.delete(`${interaction.commandName}${interaction.user.id}`)
+        }, ms(command.cooldown));
+    };
+
 
 module.exports = { executeSlashCommand };
