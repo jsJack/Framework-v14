a3a94006122e93a809fb02d8d3ce201f3684812e
Jack <hello@jackperry.co>
2024-01-21 11:48:45 +0000

    feat: ✨ Button aliases

--- structures/handlers/loadButtons.js
+++ structures/handlers/loadButtons.js
@@ -4,18 +4,26 @@ const { loadFiles } = require('../funcs/fileLoader');
 
 async function loadButtons(client) {
     await client.buttons.clear();
-    let buttonsArray = [];
+    let buttonsArray = []; let aliasArray = [];
 
     const files = await loadFiles("buttons");
     files.forEach((file) => {
         const button = require(file);
+
+        if (button.aliases) {
+            button.aliases.forEach(alias => {
+                client.buttons.set(alias, button);
+                aliasArray.push(alias + `-` + button?.id);
+            });
+        };
+
         client.buttons.set(button.id, button);
 
         buttonsArray.push(button);
     });
 
     if (!buttonsArray.length) return Logger.warn(`[Buttons] None loaded - Folder empty.`);
-    else return Logger.success(`Loaded ${cyan(`${buttonsArray.length} buttons`)}.`);
+    else return Logger.success(`Loaded ${cyan(`${buttonsArray.length} buttons`)} and ${cyan(`${aliasArray.length} aliases`)}.`);
 }
 
 module.exports = { loadButtons };
