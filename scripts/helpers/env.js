26cc4371a57f042eaa96044154b83d5e6cc57987
Jack <hello@jackperry.co>
2025-02-21 21:08:11 +0000

    feat: ✨ move env schema to helper

---
+++ scripts/helpers/env.js
@@ -0,0 +1,14 @@
+const env = {
+    DB_CONNECTION_URL: '',
+    DB_PROVIDER: '',
+
+    DISCORD_TOKEN: '',
+    SUPER_USERS: ['324596012955992065'],
+
+    DEVELOPER_GUILD_ID: '',
+    ANTICRASH_WEBHOOK: '',
+
+    NODE_ENV: 'production',
+};
+
+module.exports = { env };
--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,27 +1,16 @@
 const fs = require('fs-extra');
+
 const Logger = require('../src/structures/funcs/util/Logger');
-const createLogger = (level) => (...args) => Logger[level]('[+postinstall]', ...args);
+const { env } = require('./helpers/env')
 
 // Loggers
+const createLogger = (level) => (...args) => Logger[level]('[+postinstall]', ...args);
 const infoLog = createLogger('info');
 const warnLog = createLogger('warn');
 
 if (process.env.CI) {
     infoLog('CI detected, skipping'); // Can't rely on debug log being enabled
     process.exit(0);
-};
-
-const env = {
-    DB_CONNECTION_URL: '',
-    DB_PROVIDER: '',
-
-    DISCORD_TOKEN: '',
-    SUPER_USERS: ['324596012955992065'],
-
-    DEVELOPER_GUILD_ID: '',
-    ANTICRASH_WEBHOOK: '',
-
-    NODE_ENV: 'production',
 };
 
 if (!fs.existsSync('./.env')) {
