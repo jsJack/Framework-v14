987d309cc2cc5ec7b600af1c7e0b5fa672f6d771
Jack <hello@jackperry.co>
2025-02-21 22:05:33 +0000

    feat: ✨ db and env on startup

--- src/structures/index.js
+++ src/structures/index.js
@@ -1,13 +1,30 @@
 console.clear();
 
+const { PrismaClient } = require('@prisma/client');
 const { Client, GatewayIntentBits, Partials, Collection } = require("discord.js");
 const { Guilds, GuildMembers, GuildMessages, MessageContent, GuildVoiceStates, GuildMessageReactions } = GatewayIntentBits;
 const { User, Message, GuildMember, ThreadMember, Channel } = Partials;
 
 const Logger = require('./funcs/util/Logger');
+const { missingSecrets } = require('../../scripts/helpers/env');
 
 require('dotenv').config();
 
+/**
+ * @typedef {Object} ExtendedClient
+ * 
+ * @property {Collection} events
+ * @property {Collection} commands
+ * @property {Collection} modals
+ * @property {Collection} buttons
+ * @property {Collection} selectmenus
+ * @property {Collection} apps
+ * 
+ * @property {PrismaClient} db
+ * @property {Object} config
+ */
+
+/** @type {Client & ExtendedClient} */
 const client = new Client({
     intents: [Guilds, GuildMembers, GuildMessages, MessageContent, GuildVoiceStates, GuildMessageReactions],
     partials: [User, Message, GuildMember, ThreadMember, Channel],
@@ -19,15 +36,20 @@ client.modals = new Collection();
 client.buttons = new Collection();
 client.selectmenus = new Collection();
 client.apps = new Collection();
+
+try {
+    client.db = new PrismaClient();
+} catch (err) {
+    Logger.error(err);
+};
+
 client.config = require("./config.json");
 
 module.exports = client;
 
-let requiredSecrets = [`MONGODB_URI`, `TOKEN`, `DEVELOPER_GUILD_ID`, `ANTICRASH_WEBHOOK`];
-let missingSecrets = requiredSecrets.filter((secret) => !process.env[secret]);
-
-if (missingSecrets.length) {
-    Logger.error(`Missing required environment variables: ${missingSecrets.join(", ")}`);
+// Check for missing environment variables
+if (missingSecrets.length > 0) {
+    Logger.error(`Missing environment variables: ${missingSecrets.join(', ')}`);
     process.exit(1);
 }
 
