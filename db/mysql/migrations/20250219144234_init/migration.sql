3b38dfb6998110e7a740705e1a66248e88a35b7d
Jack <hello@jackperry.co>
2025-02-22 01:03:19 +0000

    feat: ✨ rename blacklists, add columns/types

--- db/mysql/migrations/20250219144234_init/migration.sql
+++ db/mysql/migrations/20250219144234_init/migration.sql
@@ -1,12 +1,12 @@
 -- CreateTable
-CREATE TABLE `blacklists` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `identifier` VARCHAR(191) NOT NULL,
-    `reason` VARCHAR(191) NULL,
+CREATE TABLE `Blacklist` (
+    `id` VARCHAR(18) NOT NULL,
+    `executorId` VARCHAR(18) NOT NULL,
+    `reason` LONGTEXT NULL,
     `commands` JSON NOT NULL,
     `createdAt` BIGINT NOT NULL,
     `expiresAt` BIGINT NOT NULL,
 
-    UNIQUE INDEX `blacklists_identifier_key`(`identifier`),
+    UNIQUE INDEX `Blacklist_id_key`(`id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
