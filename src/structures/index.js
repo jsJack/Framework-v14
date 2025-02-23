360811cbdc34682c7b81f96caa8629724b304bce
Jack <hello@jackperry.co>
2025-02-23 00:09:49 +0000

    style: 🎨 change order of collection decs

--- src/structures/index.js
+++ src/structures/index.js
@@ -19,11 +19,15 @@ const client = new Client({
 });
 
 client.events = new Collection();
+
 client.commands = new Collection();
-client.modals = new Collection();
-client.buttons = new Collection();
-client.selectmenus = new Collection();
 client.apps = new Collection();
+
+client.buttons = new Collection();
+client.modals = new Collection();
+client.selectmenus = new Collection();
+
+client.cooldowns = new Collection();
 
 try {
     client.db = new PrismaClient();
