682aece83a4491d438a86b9ef2914bbfd27a82d4
Jack <hello@jackperry.co>
2025-03-07 23:29:09 +0000

    feat: ✨ Embed footer handling

--- src/modals/builders/custom_embed/ceb_footer.js
+++ src/modals/builders/custom_embed/ceb_footer.js
@@ -1,4 +1,6 @@
 const { ModalSubmitInteraction, EmbedBuilder, MessageFlags } = require('discord.js');
+const timestring = require('timestring');
+
 const StatusEmbedBuilder = require('../../../structures/funcs/tools/createStatusEmbed');
 
 /** @typedef {import("../../../structures/funcs/util/Types").ExtendedClient} ExtendedClient */
@@ -41,16 +43,25 @@ module.exports = {
             });
         }
 
-        // @todo: Check if timestamp is actually a timestamp
+        if (enteredData.timestamp && !isISO8601(enteredData.timestamp)) {
+            try {
+                enteredData.timestamp = toISO8601(enteredData.timestamp);
+            } catch (e) {
+                return interaction.reply({
+                    embeds: [statusEmbed.create(e.message, 'Red')],
+                    flags: MessageFlags.Ephemeral
+                });
+            };
+        }
 
         if (enteredData.image) {
-            if (!process.env.ALLOWED_IMAGE_HOSTNAMES?.split(',').includes(new URL(iconURL).hostname) && process.env.ALLOWED_IMAGE_HOSTNAMES !== "*") {
+            if (!process.env.ALLOWED_IMAGE_HOSTNAMES?.split(',').includes(new URL(enteredData.image).hostname) && process.env.ALLOWED_IMAGE_HOSTNAMES !== "*") {
                 return interaction.reply({
                     embeds: [
                         statusEmbed
                             .create("The icon URL must be from a whitelisted domain.\n> You may be required to use a **Direct Image URL**\n> e.g. `i.imgur.com` instead of `imgur.com`", 'Red')
                             .setFields([
-                                { name: "Your URL", value: iconURL, inline: false },
+                                { name: "Your URL", value: enteredData.image, inline: false },
                                 { name: "Allowed Hostnames", value: "`" + process.env.ALLOWED_IMAGE_HOSTNAMES.split(",").join("`, `") + "`", inline: false }
                             ])
                     ],
@@ -59,7 +70,7 @@ module.exports = {
             };
 
             try {
-                const res = await fetch(iconURL, { method: 'HEAD' });
+                const res = await fetch(enteredData.image, { method: 'HEAD' });
                 const contentType = res.headers.get('content-type');
                 let supportedTypes = ['image/', 'video/'];
 
@@ -72,7 +83,7 @@ module.exports = {
                             .create("The icon URL must be a valid image URL.\n\n> **Note:** The image host must return a [MIME Type](https://developer.mozilla.org/en-US/docs/Web/HTTP/MIME_types) of `image/*` or `video/*` for the Icon URL to be accepted.", 'Red')
                             .setFields([
                                 { name: "Error", value: e.message, inline: false },
-                                { name: "Your URL", value: iconURL, inline: false }
+                                { name: "Your URL", value: enteredData.image, inline: false }
                             ])
                     ],
                     flags: MessageFlags.Ephemeral
@@ -83,7 +94,7 @@ module.exports = {
         const customEmbed = interaction.message.embeds[0];
         const instructionsEmbed = interaction.message.embeds[1];
 
-        if (referencedMessage.embeds.length != 2) {
+        if (interaction.message.embeds.length != 2) {
             return interaction.editReply({
                 embeds: [statusEmbed.create("There was an error fetching the embeds, have they been deleted?", 'Red')],
                 flags: MessageFlags.Ephemeral
@@ -109,27 +120,49 @@ module.exports = {
         }
 
         let newEmbed = EmbedBuilder.from(customEmbed);
-        let doneEmbed = statusEmbed.create("The author has been successfully updated.", 'Green');
+        let doneEmbed = statusEmbed.create("The footer has been successfully updated.", 'Green');
 
-        // const updates = [
-        //     { field: 'text', setter: 'name', value: name },
-        //     { field: 'iconURL', setter: 'icon_url', value: iconURL },
-        //     { field: 'url', setter: 'url', value: url }
-        // ];
+        const updates = [
+            { isFooter: true, field: "text", setter: "text", value: enteredData.text },
+            { isFooter: true, field: "iconURL", setter: "icon_url", value: enteredData.image },
+            { field: "timestamp", setter: "timestamp", value: enteredData.timestamp }
+        ];
 
-        // updates.forEach(({ field, setter, value }) => {
-        //     if (value == null) return;
+        updates.forEach(({ isFooter, field, setter, value }) => {
+            if (value == null) return;
 
-        //     if (value.length < 1) delete newEmbed.data.footer[setter];
-        //     newEmbed.data.author[setter] = value;
-        //     doneEmbed.addFields({ name: field.charAt(0).toUpperCase() + field.slice(1), value: value.length ? value : "> Unset", inline: false });
-        // });
+            if (isFooter) {
+                if (!newEmbed.data.footer) newEmbed.data.footer = {};
 
-        await referencedMessage.edit({ embeds: [newEmbed, instructionsEmbed] });
-        return interaction.editReply({ embeds: [doneEmbed], flags: MessageFlags.Ephemeral });
+                if (value.length < 1) delete newEmbed.data.footer[setter];
+                else newEmbed.data.footer[setter] = value;
+            } else {
+                if (value.length < 1) delete newEmbed.data[setter];
+                else newEmbed.data[setter] = value;
+            };
+
+            doneEmbed.addFields({ name: field.charAt(0).toUpperCase() + field.slice(1), value: value.length ? value : "> Unset", inline: false });
+        });
+
+        await interaction.message.edit({ embeds: [newEmbed, instructionsEmbed] });
+        return interaction.reply({ embeds: [doneEmbed], flags: MessageFlags.Ephemeral });
     }
 };
 
 function isURL(string) {
     return /^https?:\/\/(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(?:\/\S*)?$/.test(string)
+};
+
+function toISO8601(input) {
+    try {
+        const timestamp = timestring(input, 'ms'); // Convert to milliseconds
+        const date = new Date(Date.now() + timestamp); // Apply to current time if relative
+        return date.toISOString();
+    } catch (err) {
+        throw new Error("Invalid timestamp format. Please provide a valid timestamp format.");
+    }
+};
+
+function isISO8601(string) {
+    return /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{1,6})?[+-]\d{2}:\d{2}$/.test(string);
 };
