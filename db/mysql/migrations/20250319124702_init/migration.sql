4e13a566ef08421974534f401248adb65235a2ad
Jack <hello@jackperry.co>
2025-03-19 12:52:14 +0000

    tweak: ⚙️ fix migration files
    Issue with the way I setup prisma so better to reset the database and the migrations. See guide for Prisma commands.

--- db/mysql/migrations/20250319124702_init/migration.sql
+++ db/mysql/migrations/20250319124702_init/migration.sql
@@ -10,3 +10,16 @@ CREATE TABLE `Blacklist` (
     UNIQUE INDEX `Blacklist_id_key`(`id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Embed` (
+    `id` VARCHAR(19) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `createdAt` BIGINT NOT NULL,
+    `updatedAt` BIGINT NOT NULL,
+    `createdBy` VARCHAR(19) NOT NULL,
+    `embedJson` JSON NOT NULL,
+
+    UNIQUE INDEX `Embed_id_key`(`id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
